147d3d63a3204c3c2afa25f17fe65b0039d358f5
Mike Harley <mharley12345@gmail.com>
2019-09-13 10:57:26 -0700

    adds reducer

---
+++ smurfs/src/actions/index.js
@@ -0,0 +1,21 @@
+import axios from "axios"
+export const FETCH_SMURFS_DATA_START = "FETCH_SMURFS_DATA_START";
+export const FETCH_SMURFS_DATA_SUCCESS = "FETCH_SMURFS_DATA_SUCCESS";
+export const FETCH_SMURFS_DATA_FAILURE = "FETCH_SMURFS_DATA_FAILURE";
+
+
+export const getData = () => {
+    return dispatch => {
+        dispatch({ type : FETCH_SMURFS_DATA_START});
+        axios 
+            .get('http://localhost:3333/smurfs')
+            .then(res=>{
+                console.log("GET Reguest Response", res);
+                dispatch({ type: FETCH_SMURFS_DATA_SUCCESS, payload: res.data});
+
+            })
+            .catch(err =>{
+                dispatch({ type: FETCH_SMURFS_DATA_FAILURE, payload: err.response})
+            });
+    };
+};
